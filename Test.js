d2d2d420a78c2686818e0893446e4bbfdcf9dbba
AkiArasaki-PC <tjy1367241585@gmail.com>
2024-12-28 04:19:45 -0500

    pcm directly to text by using Promise

--- Test.js
+++ Test.js
@@ -1,10 +1,6 @@
-const {toMp3, convert} = require("./utils/PcmToText");
+const {convert} = require("./utils/PcmToText");
 
-const path = "records/713628281118916698-1735374494412.pcm";
-toMp3(path).then(r => {
-    console.log(r.toString());
-})
-
-convert("records/713628281118916698-1735374494412.mp3").then(r => {
-    console.log(r.toString());
-})
+const path = "records/test1.pcm";
+convert(path).then(r =>{
+    console.log(r);
+});
--- utils/PcmToText.js
+++ utils/PcmToText.js
@@ -7,20 +7,38 @@ const {groqApiKey} = require('../config.json');
 const groqTest = new Groq({ apiKey: groqApiKey });
 ffmpeg.setFfmpegPath(ffmpegPath);
 
-async function toMp3(path) {
-    ffmpeg(path)
-        .inputFormat('s32le')
-        .audioCodec('libmp3lame')
-        .audioBitrate('320k')
-        .audioChannels(2)
-        .audioFrequency(48000)
-        .save(path.split('.')[0] + '.mp3');
-    return path.split('.')[0] + '.mp3';
+async function convert(path) {
+    return new Promise((resolve) => {
+        toMp3(path).then(stream => {
+            toText(stream).then(data => {
+                resolve(data)
+            });
+        });
+    });
 }
 
-async function convert(path) {
+async function toMp3(path) {
+    return new Promise((resolve, reject) => {
+        const outputPath = path.split('.')[0] + '.mp3';
+        ffmpeg(path)
+            .inputFormat('s32le')
+            .audioCodec('libmp3lame')
+            .audioBitrate('320k')
+            .audioChannels(2)
+            .audioFrequency(48000)
+            .save(outputPath)
+            .on('end', () => {
+                resolve(fs.createReadStream(outputPath));
+            })
+            .on('error', (err) => {
+                reject(err);
+            });
+    });
+}
+
+async function toText(stream) {
     const transcription = await groqTest.audio.transcriptions.create({
-        file: fs.createReadStream(path),
+        file: stream,
         model: "whisper-large-v3-turbo",
         prompt: "Specify context or spelling",
         response_format: "json",
@@ -31,6 +49,5 @@ async function convert(path) {
 }
 
 module.exports = {
-    toMp3,
     convert
 }
