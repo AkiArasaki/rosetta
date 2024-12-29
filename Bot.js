ff3bb128e76e167ec504a331e19eaf768b4425d8
aki-laptop <tjy1367241585@gmail.com>
2024-12-29 01:45:35 -0500

    auto convert to text

--- Bot.js
+++ Bot.js
@@ -1,6 +1,8 @@
 const fs = require('node:fs');
 const { joinVoiceChannel } = require('@discordjs/voice');
 const prism = require('prism-media');
+const {convert} = require("./utils/PcmToText");
+const {sleep} = require("groq-sdk/core");
 
 class Bot {
     constructor() {
@@ -18,23 +20,35 @@ class Bot {
             selfDeaf: false
         });
         this.connection[id].receiver.speaking.on('start', (userId) => {
-            console.log(userId+" started")
-            this.writeStream[id] = fs.createWriteStream(`records/${userId}-${Date.now()}.pcm`);
+            if (userId !== "713628281118916698") return;
+            //console.log(userId+" started")
+            const path = `records/${userId}-${Date.now()}.pcm`;
+            this.writeStream[id] = fs.createWriteStream(path);
             this.listenStream[id] = this.connection[id].receiver.subscribe(userId);
-
             const opusDecoder = new prism.opus.Decoder({
                 frameSize: 960,
                 channels: 2,
                 rate: 48000,
             });
             this.listenStream[id].pipe(opusDecoder).pipe(this.writeStream[id]);
+            console.log(this.listenStream[id].length)
+            this.writeStream[id].on('finish', () => {
+                convert(path).then(r =>{
+                    console.log(r);
+                    interaction.channel.send(r);
+                });
+            });
         });
 
-        this.connection[id].receiver.speaking.on('end', (userId) => {
+        this.connection[id].receiver.speaking.on('end', () => {
             //actions here
-            console.log(userId+" ended")
-            this.writeStream[id].destroy();
-            this.listenStream[id].destroy();
+            //console.log(userId+" ended")
+            sleep(7000);
+            if (this.writeStream[id]) {
+                this.writeStream[id].end();
+                this.writeStream[id] = null;
+                this.listenStream[id] = null;
+            }
         });
     }
 
--- Test.js
+++ Test.js
@@ -1,6 +1,6 @@
 const {convert} = require("./utils/PcmToText");
 
-const path = "records/test1.pcm";
+const path = "records/713628281118916698-1735378456333.pcm";
 convert(path).then(r =>{
     console.log(r);
 });
